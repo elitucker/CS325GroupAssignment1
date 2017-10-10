d2462004527374911803abf5812ac257bf56d1df
Jarrod Hollis <hollisj@oregonstate.edu>
2017-10-10 12:51:54 -0700

    Almost done!

--- select.cpp
+++ select.cpp
@@ -3,14 +3,16 @@
 #include<fstream>
 #include<vector>
 #include<tuple>
+#include<climits>
 
 #define BYTE_COUNT 4
 
 using namespace std;
-typedef tuple<int, int> i2tuple;
+typedef tuple<uint, uint> i2tuple;
 
-int recurse(vector<ifstream*>, vector<i2tuple>, int, int);
-int binarySearch(ifstream *datFile, i2tuple indices, int midpoint);
+uint recurse(vector<ifstream*>, vector<i2tuple>, uint, uint);
+uint binarySearch(ifstream *datFile, i2tuple indices, uint midpoint);
+uint swapByteOrder(uint num);
 
 int main()
 {
@@ -18,10 +20,10 @@ int main()
 	vector <ifstream*> datFiles; //???
 	vector <i2tuple> idxList;
 	ofstream output;
-	int numOfFiles;
-	int numOfCols;
-	int k; //kth smallest index
-	int n;
+	uint numOfFiles;
+	uint numOfCols;
+	uint k; //kth smallest index
+	uint n;
 
 	//open input.txt and retrieve necessary variables
 	input.open("input.txt");
@@ -53,7 +55,7 @@ int main()
 	n = numOfFiles * numOfCols;
 
 	//use first number from input.txt to obtain number of .dat files
-	for (int i = 1; i <= numOfFiles; i++)
+	for (uint i = 1; i <= numOfFiles; i++)
 	{
 		//add each file to the vector of ifstreams in the format of "1.dat"
 		ifstream *file = new ifstream(to_string(i) + ".dat", ios::binary);
@@ -66,39 +68,53 @@ int main()
 	return 0;
 }
 
-int recurse(vector<ifstream*> datFiles, vector<i2tuple> idxList, int k, int n)
+uint recurse(vector<ifstream*> datFiles, vector<i2tuple> idxList, uint k, uint n)
 {
 
-	int midpoint;
+	uint midpoint;
 
-	if (n == 1)
+	if (k == 1)
 	{
-		for (int i = 0; i < datFiles.size(); i++)
+		uint value = UINT_MAX;
+		for (uint i = 0; i < datFiles.size(); i++)
 		{
 			if (get<0>(idxList[i]) <= get<1>(idxList[i]))
 			{
 				(*datFiles[i]).seekg(get<0>(idxList[i]) * BYTE_COUNT).read((char *)&midpoint, BYTE_COUNT);
-				return midpoint;
+				midpoint = swapByteOrder(midpoint);
+				value = (midpoint < value ? midpoint : value);
 			}
-			return -1;
+		}
+		return value;
+	}
+
+	vector<uint> midIndices; //holds the temporary middle idx values
+
+	uint idx;
+
+	//find the midpoint of the topmost vector
+	for (idx = 0; idx < datFiles.size(); idx++)
+	{
+		if (get<0>(idxList[idx]) <= get<1>(idxList[idx]))
+		{
+			(*(datFiles[0])).seekg(((get<1>(idxList[idx]) - get<0>(idxList[idx])) / 2 + get<0>(idxList[idx])) * BYTE_COUNT).read((char *)&midpoint, BYTE_COUNT);
+			midpoint = swapByteOrder(midpoint);
+			midIndices.push_back((get<1>(idxList[idx]) - get<0>(idxList[idx])) / 2 + get<0>(idxList[idx]));
+			break;
 		}
 	}
 
-	vector<int> midIndices; //holds the temporary middle idx values
-	int greater = 0;
-	int lesser = 0;
-
-	//find the midpoint of the topmost vector
-	(*(datFiles[0])).seekg(((get<1>(idxList[0]) - get<0>(idxList[0])) / 2) * BYTE_COUNT).read((char *)&midpoint, BYTE_COUNT);
+	uint lesser = (get<1>(idxList[idx]) - get<0>(idxList[idx])) / 2;
+	uint greater = get<1>(idxList[idx]) - get<0>(idxList[idx]) - lesser + 1;
 
 	//count how many idx's are lesser and greater than the midpoint
-	for (int i = 0; i < datFiles.size(); i++)
+	for (idx++; idx < datFiles.size(); idx++)
 	{
-		if (get<0>(idxList[i]) <= get<1>(idxList[i]))
+		if (get<0>(idxList[idx]) <= get<1>(idxList[idx]))
 		{
-			int mid = binarySearch(datFiles[i], idxList[i], midpoint);
-			lesser += mid - get<0>(idxList[i]);
-			greater += get<1>(idxList[i]) -  mid + 1;
+			uint mid = binarySearch(datFiles[idx], idxList[idx], midpoint);
+			lesser += mid - get<0>(idxList[idx]);
+			greater += get<1>(idxList[idx]) -  mid + 1;
 			midIndices.push_back(mid);
 		}
 	}
@@ -106,18 +122,24 @@ int recurse(vector<ifstream*> datFiles, vector<i2tuple> idxList, int k, int n)
 	//use above information to determine
 	if (k > lesser)
 	{
-		for (int i = 0; i < idxList.size(); i++)
+		for (uint i = 0; i < idxList.size(); i++)
 		{
-			get<0>(idxList[i]) = midIndices[i];
+			if (get<0>(idxList[i]) <= get<1>(idxList[i]))
+			{
+				get<0>(idxList[i]) = midIndices[i];
+			}
 		}
 		k -= lesser;
 		n -= lesser;
 	}
 	else
 	{
-		for (int i = 0; i < idxList.size(); i++)
+		for (uint i = 0; i < idxList.size(); i++)
 		{
-			get<1>(idxList[i]) = midIndices[i] - 1;
+			if (get<0>(idxList[i]) <= get<1>(idxList[i]))
+			{
+				get<1>(idxList[i]) = midIndices[i] - 1;
+			}
 		}
 		n -= greater;
 	}
@@ -125,15 +147,18 @@ int recurse(vector<ifstream*> datFiles, vector<i2tuple> idxList, int k, int n)
 	return recurse(datFiles, idxList, k, n);
 }
 
-int binarySearch(ifstream *datFile, i2tuple indices, int midpoint)
+uint binarySearch(ifstream *datFile, i2tuple indices, uint midpoint)
 {
-	int low = get<0>(indices);
-	int high = get<1>(indices);
-	int mid = 0;
+	uint low = get<0>(indices);
+	uint high = get<1>(indices);
+	uint mid = 0;
 	while (low != high) //when low == high, both hold index of smallest value >= midpoint
 	{
+		uint compare;
 		mid = (low + high) / 2;
-		if ((*datFile).seekg(mid).get() < midpoint)
+		(*datFile).seekg(mid * BYTE_COUNT).read((char *)&compare, BYTE_COUNT);
+		compare = swapByteOrder(compare);
+		if (compare < midpoint)
 		{
 			low = mid + 1;
 		}
@@ -145,72 +170,8 @@ int binarySearch(ifstream *datFile, i2tuple indices, int midpoint)
 	return low;
 }
 
-/*
-
-pick an array
-middle element of largest array
-binary search through other arrays to find that value or value <
-find indexes of those elements
-
-k = 10
-
-M = 3
-
-1, 2, 3, 4, 5
-2, 4, 6, 8, 10
-3, 6, 9, 12, 15
-
-3 + 1 + 1 = 5
-2 + 4 + 4 = 10
-
-10 - 5 = 5 = k
-
-M = 4
-
-4, 5
-4, 6, 8, 10
-6, 9, 12, 15
-
-1 + 1 + 0 = 2
-1 + 3 + 4 = 8
-
-5 - 2 = 3 = k
-
-M = 5
-
-5
-6, 8, 10
-6, 9, 12, 15
-
-1 + 0 + 0 = 1
-0 + 3 + 4 = 7
-
-3 - 1 = 2 = k
-
-M = 8
-
-6, 8, 10
-6, 9, 12, 15
-
-2 + 1 + 0 = 3
-1 + 3 + 0 = 4
-
-if k < L then eliminate G
-DO NOT REDUCE K AT THIS POINT
-
-lower idx = 3
-upper idx = 4
-6, 8
-6
-
-M = 6
-
-1 + 1 = 2
-1 + 0 = 1
-
-2 - 1 = 1 = k
-
-6
-6
-
-*/
+uint swapByteOrder(uint num)
+{
+	char *bytes = (char *) &num;
+	return (bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3]);
+}
